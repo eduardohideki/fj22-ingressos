a19414bee00f45780b03a9a4a951cfd88fa3e54d
= <=>
2019-10-01 19:41:19 -0300

    Criando tela de detalhes dos filmes

--- src/main/java/br/com/caelum/ingresso/dao/SessaoDao.java
+++ src/main/java/br/com/caelum/ingresso/dao/SessaoDao.java
@@ -4,6 +4,10 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.com.caelum.ingresso.model.Filme;
 import br.com.caelum.ingresso.model.Sala;
 import br.com.caelum.ingresso.model.Sessao;
 
@@ -21,4 +25,8 @@ public class SessaoDao {
 		return manager.createQuery("select s from Sessao s where s.sala = :sala", Sessao.class).setParameter("sala", sala).getResultList();		
 	}
 
+	public List<Sessao> buscaSessoesDoFilme(Filme filme){
+    	return manager.createQuery("select s from Sessao s where s.filme = :filme", Sessao.class).setParameter("filme", filme).getResultList();
+    }
+    
 }
